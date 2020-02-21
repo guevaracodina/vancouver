79ec3ae1c6536f4895584704960ba1a9f68db3fe
Edgar Guevara <edgar.guevara@polymtl.ca>
2020-02-20 19:49:53 -0600

    removeNaN.m - Fixed bug when NaN was the last element of the vector, also avoids whole vectors filled with NaNs
    Signed-off-by: Edgar Guevara <edgar.guevara@polymtl.ca>

--- removeNaN.m
+++ removeNaN.m
@@ -1,8 +1,8 @@
 function raman = removeNaN(raman)
 % Remove NaN's from column
 idxNaN = find(isnan(raman));        % Find & Remove Nan's
-if ~isempty(idxNaN)
-    if any(idxNaN) == 1
+if ~isempty(idxNaN) && ~(numel(idxNaN) == numel(raman))
+    if any(idxNaN) == 1 && (all(idxNaN) ~= numel(raman))
         raman(idxNaN) = raman(idxNaN+1);
     elseif any(idxNaN) == numel(raman)
         raman(idxNaN) = raman(idxNaN-1);
